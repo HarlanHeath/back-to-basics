fecc8182991050d9122191580721ffebd9a4f5b2
User <user@Users-MacBook-Pro.local>
2019-11-22 14:40:59 -0600

    building out some componentes

---
+++ back-to-basics/src/Components/Person/Person.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Person = () => {
+    return (
+        <div>
+            <p> I'm a person and I am {Math.floor(Math.random() * 90)} years old!</p>
+        </div>
+    )
+}
+
+export default Person

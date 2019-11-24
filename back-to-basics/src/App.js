89bccf3e6b6ee6c3eae5220d55b1bd168385f5ca
Harlan Heath <harlandheath@gmail.com>
2019-11-23 20:27:06 -0600

    passing props

--- back-to-basics/src/App.js
+++ back-to-basics/src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import Person from "./Components/Person/Person"
-import './App.css';
+import React, { Component } from "react";
+import Person from "./Components/Person/Person";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App" >
+      <div className="App">
         <h1>Harlan's Here</h1>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Harlan" age="30">
+          I love WOW
+        </Person>
+        <Person name="Har" age="35" />
+        <Person name="Lan" age="40" />
       </div>
     );
   }
--- back-to-basics/src/Components/Person/Person.js
+++ back-to-basics/src/Components/Person/Person.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-const Person = () => {
-    return (
-        <div>
-            <p> I'm a person and I am {Math.floor(Math.random() * 90)} years old!</p>
-        </div>
-    )
-}
+const Person = props => {
+  return (
+    <div>
+      <p>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
+};
 
-export default Person
+export default Person;
